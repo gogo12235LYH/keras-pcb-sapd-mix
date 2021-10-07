3a09c4a377830e5dc8d8051cd6022a9f546bb411
gogo12235LYH <dogfish87@gmail.com>
2021-10-07 18:58:55 +0800

    update callback.py * move method from train.py * fixed some error

--- callback.py
+++ callback.py
@@ -1,127 +1,201 @@
+import numpy as np
+import tensorflow as tf
 import tensorflow.keras as keras
-from utils.eval import evaluate
+import tensorflow.keras.backend as k
+from tensorflow.keras.callbacks import LearningRateScheduler
+from eval.voc import Evaluate
 
 
-class Evaluate(keras.callbacks.Callback):
-    """
-    Evaluation callback for arbitrary datasets.
-    """
-
-    def __init__(
-            self,
-            generator,
-            iou_threshold=0.5,
-            score_threshold=0.05,
-            max_detections=100,
-            save_path=None,
-            tensorboard=None,
-            weighted_average=False,
-            verbose=1
-    ):
-        """
-        Evaluate a given dataset using a given model at the end of every epoch during training.
-        Args:
-            generator: The generator that represents the dataset to evaluate.
-            iou_threshold: The threshold used to consider when a detection is positive or negative.
-            score_threshold: The score confidence threshold to use for detections.
-            max_detections: The maximum number of detections to use per image.
-            save_path: The path to save images with visualized detections to.
-            tensorboard: Instance of keras.callbacks.TensorBoard used to log the mAP value.
-            weighted_average: Compute the mAP using the weighted average of precisions among classes.
-            verbose: Set the verbosity level, by default this is set to 1.
-        """
-        self.generator = generator
-        self.iou_threshold = iou_threshold
-        self.score_threshold = score_threshold
-        self.max_detections = max_detections
-        self.save_path = save_path
-        self.tensorboard = tensorboard
-        self.weighted_average = weighted_average
-        self.verbose = verbose
-
-        super(Evaluate, self).__init__()
-
-    def on_epoch_end(self, epoch, logs=None):
-        logs = logs or {}
-
-        # run evaluation
-        average_precisions = evaluate(
-            self.generator,
-            self.model,
-            iou_threshold=self.iou_threshold,
-            score_threshold=self.score_threshold,
-            max_detections=self.max_detections,
-            save_path=self.save_path,
-            epoch=epoch
-        )
-
-        # compute per class average precision
-        total_instances = []
-        precisions = []
-        for label, (average_precision, num_annotations) in average_precisions.items():
-            if self.verbose == 1:
-                print('{:.0f} instances of class'.format(num_annotations),
-                      self.generator.label_to_name(label), 'with average precision: {:.4f}'.format(average_precision))
-            total_instances.append(num_annotations)
-            precisions.append(average_precision)
-        if self.weighted_average:
-            self.mean_ap = sum([a * b for a, b in zip(total_instances, precisions)]) / sum(total_instances)
-        else:
-            self.mean_ap = sum(precisions) / sum(x > 0 for x in total_instances)
-
-        if self.tensorboard is not None and self.tensorboard.writer is not None:
-            import tensorflow as tf
-            summary = tf.Summary()
-            summary_value = summary.value.add()
-            summary_value.simple_value = self.mean_ap
-            summary_value.tag = "mAP"
-            self.tensorboard.writer.add_summary(summary, epoch)
-
-        logs['mAP'] = self.mean_ap
-
-        if self.verbose == 1:
-            print('mAP: {:.4f}'.format(self.mean_ap))
-
-
-class RedirectModel(keras.callbacks.Callback):
-    """
-    Callback which wraps another callback, but executed on a different model.
-    ```python
-    model = keras.models.load_model('model.h5')
-    model_checkpoint = ModelCheckpoint(filepath='snapshot.h5')
-    parallel_model = multi_gpu_model(model, gpus=2)
-    parallel_model.fit(X_train, Y_train, callbacks=[RedirectModel(model_checkpoint, model)])
-    ```
-    Args
-        callback : callback to wrap.
-        model : model to use when executing callbacks.
-    """
-
-    def __init__(self,
-                 callback,
-                 model):
-        super(RedirectModel, self).__init__()
-
-        self.callback = callback
-        self.redirect_model = model
+class History(tf.keras.callbacks.Callback):
+    def __init__(self, lr_scheduler=None, wd_scheduler=None):
+        super(History, self).__init__()
+        self.lr_scheduler = lr_scheduler
+        self.wd_scheduler = wd_scheduler
 
     def on_epoch_begin(self, epoch, logs=None):
-        self.callback.on_epoch_begin(epoch, logs=logs)
+        if self.lr_scheduler:
+            k.set_value(self.model.optimizer.lr, self.lr_scheduler(epoch))
 
-    def on_epoch_end(self, epoch, logs=None):
-        self.callback.on_epoch_end(epoch, logs=logs)
+        lr = float(k.get_value(self.model.optimizer.lr))
 
-    def on_batch_begin(self, batch, logs=None):
-        self.callback.on_batch_begin(batch, logs=logs)
+        if self.wd_scheduler:
+            try:
+                k.set_value(self.model.optimizer.weight_decay, self.wd_scheduler(epoch))
+                wd = float(k.get_value(self.model.optimizer.weight_decay))
+                print(f"[INFO] From History Callback: EP:{epoch} LR: {lr}, WD: {wd}")
+            except:
+                print(f"[INFO] From History Callback: EP:{epoch} LR: {lr}")
 
-    def on_batch_end(self, batch, logs=None):
-        self.callback.on_batch_end(batch, logs=logs)
 
-    def on_train_begin(self, logs=None):
-        # overwrite the model with our custom model
-        self.callback.set_model(self.redirect_model)
+def create_callbacks(
+        config,
+        pred_mod,
+        test_gen,
+        tensorboard=0,
+        tensorboard_path=None,
+):
+    cbs = []
+    info_ = ''
 
-        self.callback.on_train_begin(logs=logs)
+    if config.EVALUATION:
+        info_ = info_ + "Evaluation, "
+        cbs.append(
+            Evaluate(test_gen, pred_mod)
+        )
 
-    def on_train_end(self, logs=None):
-        self.callback.on_train_end(logs=logs)
+    if tensorboard:
+        info_ = info_ + "Tensorboard, "
+
+        cbs.append(
+            keras.callbacks.TensorBoard(
+                log_dir='{file_path}/logs'.format(file_path=tensorboard_path),
+                histogram_freq=0,
+                write_graph=True,
+                write_images=False,
+                profile_batch=config.BATCH_SIZE,
+                embeddings_freq=0,
+                embeddings_metadata=None
+            )
+        )
+
+    # if config.LR_Scheduler == 1:
+    #     info_ = info_ + "Cosine-Decay, "
+    #     cbs.append(
+    #         lr_cosine_decay(
+    #             initial_lr=config.BASE_LR,
+    #             epochs=config.EPOCHs,
+    #             warm_up=config.USING_WARMUP,
+    #             warm_up_epochs=config.WP_EPOCHs,
+    #             warm_up_ratio=config.WP_RATIO,
+    #             alpha=config.ALPHA
+    #         )
+    #     )
+
+    if config.USING_HISTORY == 1:
+        info_ = info_ + "History, "
+
+        _decay_dict = {
+            "total_epochs": config.EPOCHs,
+            "warm_up": config.USING_WARMUP,
+            "warm_up_epochs": config.WP_EPOCHs,
+            "warm_up_ratio": config.WP_RATIO,
+            "alpha": config.ALPHA,
+        }
+
+        cbs.append(
+            History(
+                lr_scheduler=cosine_decay_scheduler(
+                    param="LR",
+                    init_val=config.BASE_LR,
+                    **_decay_dict
+                ),
+                wd_scheduler=cosine_decay_scheduler(
+                    param="WD",
+                    init_val=config.DECAY,
+                    **_decay_dict
+                )
+            )
+        )
+
+    # Early stopping.
+    cbs.append(
+        keras.callbacks.EarlyStopping(
+            monitor='loss',
+            mode='min',
+            patience=3,
+            restore_best_weights=True,
+            verbose=1,
+        )
+    )
+    print(info_ + 'EarlyStopping')
+    return cbs
+
+
+def wd_cosine_decay(epochs, initial_lr=1e-4, warm_up=0, warm_up_epochs=0, warm_up_ratio=0.1, alpha=0.):
+    eps = (epochs - warm_up_epochs) - 1 if warm_up else epochs
+    min_lr = warm_up_ratio * initial_lr
+
+    def warm_up_lr(epoch):
+        decay = min_lr + (initial_lr - min_lr) * (epoch / warm_up_epochs)
+        print(f'-- [INFO] Warmup ({epoch + 1}/{warm_up_epochs}) WD : {decay}')
+        return decay
+
+    def cosine_decay(epoch):
+        ep = (epoch - warm_up_epochs) if warm_up else epoch
+        cosine = 0.5 * (1 + np.cos(np.pi * ep / eps))
+        cosine = (1 - alpha) * cosine + alpha
+        decay = initial_lr * cosine
+        print(f'-- [INFO] Cosine-Decay WD : {decay}')
+        return decay
+
+    def lr_scheduler(epoch):
+        decay = warm_up_lr(epoch) if warm_up and epoch < warm_up_epochs else cosine_decay(epoch)
+        return decay
+
+    return lr_scheduler
+
+
+def lr_cosine_decay(epochs, initial_lr=1e-4, warm_up=0, warm_up_epochs=0, warm_up_ratio=0.1, alpha=0.):
+    eps = (epochs - warm_up_epochs) - 1 if warm_up else epochs
+    min_lr = warm_up_ratio * initial_lr
+
+    def warm_up_lr(epoch):
+        decay = min_lr + (initial_lr - min_lr) * (epoch / warm_up_epochs)
+        print(f'-- [INFO] Warmup ({epoch + 1}/{warm_up_epochs}) LR : {decay}')
+        return decay
+
+    def cosine_decay(epoch):
+        ep = (epoch - warm_up_epochs) if warm_up else epoch
+        cosine = 0.5 * (1 + np.cos(np.pi * ep / eps))
+        cosine = (1 - alpha) * cosine + alpha
+        decay = initial_lr * cosine
+        print(f'-- [INFO] Cosine-Decay LR : {decay}')
+        return decay
+
+    def lr_scheduler(epoch, lr):
+        decay = warm_up_lr(epoch) if warm_up and epoch < warm_up_epochs else cosine_decay(epoch)
+        return decay
+
+    return LearningRateScheduler(lr_scheduler)
+
+
+def cosine_decay_scheduler(
+        param: str,
+        total_epochs=100,
+        init_val=1e-4,
+        warm_up=0,
+        warm_up_epochs=0,
+        warm_up_ratio=0.1,
+        alpha=0.,
+        using_keras=0,
+):
+    t_eps = (total_epochs - warm_up_epochs) - 1 if warm_up else total_epochs
+    min_val = warm_up_ratio * init_val
+
+    def _warm_up(current_epoch):
+        decay = min_val + (init_val - min_val) * (current_epoch / warm_up_epochs)
+        print(f'-- [INFO] Warmup ({current_epoch + 1}/{warm_up_epochs}) {param} : {decay}')
+        return decay
+
+    def _cosine_decay(current_epoch):
+        c_ep = (current_epoch - warm_up_epochs) if warm_up else current_epoch
+        cosine = 0.5 * (1 + np.cos(np.pi * c_ep / t_eps))
+        cosine = (1 - alpha) * cosine + alpha
+        decay = init_val * cosine
+        print(f'-- [INFO] Cosine-Decay {param} : {decay}')
+        return decay
+
+    def _output(epoch):
+        return _warm_up(epoch) if warm_up and epoch < warm_up_epochs else _cosine_decay(epoch)
+
+    if using_keras:
+        def _scheduler(epoch, lr):
+            return _output(epoch)
+
+        return LearningRateScheduler(_scheduler)
+
+    else:
+        def _scheduler(epoch):
+            return _output(epoch)
+
+        return _scheduler

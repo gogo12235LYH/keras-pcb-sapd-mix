0acee93f68aece452805d791c5d3a063e8c96689
gogo12235LYH <dogfish87@gmail.com>
2021-10-19 17:37:20 +0800

    update pipeline.py * adding rotate augmentation.

--- generators/pipeline.py
+++ generators/pipeline.py
@@ -41,85 +41,38 @@ def _fmap_shapes(phi: int = 0, level: int = 5):
     return shapes
 
 
-def random_flip_horizontal(image, boxes):
-    """Flips image and boxes horizontally with 50% chance
+def random_flip_horizontal(
+        image, image_shape, bboxes, prob=0.5
+):
+    """Flips image and boxes horizontally
 
     Arguments:
       image: A 3-D tensor of shape `(height, width, channels)` representing an
         image.
-      boxes: A tensor with shape `(num_boxes, 4)` representing bounding boxes,
+      image_shape:
+      bboxes: A tensor with shape `(num_boxes, 4)` representing bounding boxes,
         having normalized coordinates.
+      prob: Chance.
 
     Returns:
       Randomly flipped image and boxes
     """
-    if tf.random.uniform(()) > 0.5:
+    if tf.random.uniform(()) > prob:
         image = tf.image.flip_left_right(image)
-        boxes = tf.stack(
-            [1 - boxes[:, 2],
-             boxes[:, 1],
-             1 - boxes[:, 0],
-             boxes[:, 3]]
+        bboxes = tf.stack(
+            [image_shape[1] - bboxes[:, 2],
+             bboxes[:, 1],
+             image_shape[1] - bboxes[:, 0],
+             bboxes[:, 3]]
             , axis=-1
         )
-    return image, boxes
+    return image, bboxes
 
 
 @tf.function
-def rotate(image, bboxes, prob=1.0, border_value=0.):
-    random_prob = np.random.uniform()
-    if random_prob < (1 - prob):
-        return image, bboxes
-
-    rotate_degree = np.random.choice([90, 180, 270])
-    h, w = image.shape[:2]
-
-    # Compute the rotation matrix.
-    M = cv2.getRotationMatrix2D(center=(w / 2, h / 2),
-                                angle=rotate_degree,
-                                scale=1)
-
-    # Get the sine and cosine from the rotation matrix.
-    abs_cos_angle = np.abs(M[0, 0])
-    abs_sin_angle = np.abs(M[0, 1])
-
-    # Compute the new bounding dimensions of the image.
-    new_w = int(h * abs_sin_angle + w * abs_cos_angle)
-    new_h = int(h * abs_cos_angle + w * abs_sin_angle)
-
-    # Adjust the rotation matrix to take into account the translation.
-    M[0, 2] += new_w // 2 - w // 2
-    M[1, 2] += new_h // 2 - h // 2
-
-    # Rotate the image.
-    image = cv2.warpAffine(image, M=M, dsize=(new_w, new_h), flags=cv2.INTER_CUBIC,
-                           borderMode=cv2.BORDER_CONSTANT,
-                           borderValue=border_value)
-
-    new_bboxes = []
-
-    if bboxes.shape[0] != 0:
-        for bbox in bboxes:
-            x1, y1, x2, y2 = bbox
-            points = M.dot([
-                [x1, x2, x1, x2],
-                [y1, y2, y2, y1],
-                [1, 1, 1, 1],
-            ])
-            # Extract the min and max corners again.
-            min_xy = np.sort(points, axis=1)[:, :2]
-            min_x = np.mean(min_xy[0])
-            min_y = np.mean(min_xy[1])
-            max_xy = np.sort(points, axis=1)[:, 2:]
-            max_x = np.mean(max_xy[0])
-            max_y = np.mean(max_xy[1])
-            new_bboxes.append([min_x, min_y, max_x, max_y])
-
-    return image, new_bboxes
-
-
-@tf.function
-def tf_rotate(image, bboxes, image_shape, prob=0.5):
+def tf_rotate(
+        image, image_shape, bboxes, prob=0.5
+):
     offset = image_shape / 2.
     rotate_k = np.random.choice([1, 2, 3])
 
@@ -132,8 +85,8 @@ def tf_rotate(image, bboxes, image_shape, prob=0.5):
 
         offset_h, offset_w = offset[0], offset[1]
 
-        new_offset_w = offset_w * (tf_abs_cos - tf_cos) + offset_h * (tf_abs_sin * tf_sin)
-        new_offset_h = offset_w * (tf_abs_sin + tf_sin) + offset_h * (tf_abs_cos * tf_cos)
+        new_offset_w = offset_w * (tf_abs_cos - tf_cos) + offset_h * (tf_abs_sin - tf_sin)
+        new_offset_h = offset_w * (tf_abs_sin + tf_sin) + offset_h * (tf_abs_cos - tf_cos)
 
         x_r = x * tf_cos + y * tf_sin + new_offset_w
         y_r = x * tf_sin * -1 + y * tf_cos + new_offset_h
@@ -146,17 +99,16 @@ def tf_rotate(image, bboxes, image_shape, prob=0.5):
 
         x1, y1, x2, y2 = bbox[0], bbox[1], bbox[2], bbox[3]
 
-        x1, y1 = _r_method(x1, y1, angle)
-        x2, y2 = _r_method(x2, y2, angle)
+        x1_n, y1_n = _r_method(x1, y1, angle)
+        x2_n, y2_n = _r_method(x2, y2, angle)
 
         bbox = tf.stack([
-            tf.minimum(x1, x2), tf.minimum(y1, y2),
-            tf.maximum(x1, x2), tf.maximum(y1, y2)
+            tf.minimum(x1_n, x2_n), tf.minimum(y1_n, y2_n),
+            tf.maximum(x1_n, x2_n), tf.maximum(y1_n, y2_n)
         ])
         return bbox
 
     if tf.random.uniform(()) > prob:
-
         image = tf.image.rot90(image, k=rotate_k)
 
         bboxes = tf.map_fn(
@@ -168,34 +120,13 @@ def tf_rotate(image, bboxes, image_shape, prob=0.5):
     return image, bboxes
 
 
-@tf.function
-def crop(image, bboxes, prob=0.5):
-    if tf.random.uniform(()) > prob:
-
-        h, w = image.shape[:2]
-
-        if bboxes.shape[0] != 0:
-            min_x1, min_y1 = np.min(bboxes, axis=0)[:2]
-            max_x2, max_y2 = np.max(bboxes, axis=0)[2:]
-            random_x1 = np.random.randint(0, max(min_x1 // 2, 1))
-            random_y1 = np.random.randint(0, max(min_y1 // 2, 1))
-            random_x2 = np.random.randint(max_x2 + 1, max(min(w, max_x2 + (w - max_x2) // 2), max_x2 + 2))
-            random_y2 = np.random.randint(max_y2 + 1, max(min(h, max_y2 + (h - max_y2) // 2), max_y2 + 2))
-            image = image[random_y1:random_y2, random_x1:random_x2]
-            bboxes[:, [0, 2]] = bboxes[:, [0, 2]] - random_x1
-            bboxes[:, [1, 3]] = bboxes[:, [1, 3]] - random_y1
-
-        else:
-            random_x1 = np.random.randint(0, max(w // 8, 1))
-            random_y1 = np.random.randint(0, max(h // 8, 1))
-            random_x2 = np.random.randint(7 * w // 8, w - 1)
-            random_y2 = np.random.randint(7 * h // 8, h - 1)
-            image = image[random_y1:random_y2, random_x1:random_x2]
-
-    return image, bboxes
-
-
-def preprocess_data(phi=0, mode: str = "ResNetV1", fmap_shapes=None, max_bboxes: int = 100, padding_value: float = 0.):
+def preprocess_data(
+        phi: int = 0,
+        mode: str = "ResNetV1",
+        fmap_shapes: any = None,
+        max_bboxes: int = 100,
+        padding_value: float = 0.
+):
     """Applies preprocessing step to a single sample
 
     ref: https://keras.io/examples/vision/retinanet/#preprocessing-data
@@ -227,15 +158,15 @@ def preprocess_data(phi=0, mode: str = "ResNetV1", fmap_shapes=None, max_bboxes:
 
         return image, scale, offset_h, offset_w
 
-    def _padding_bboxes(image_shape, bboxes, classes, scale, offset_h, offset_w):
+    def _padding_bboxes(bboxes, classes, scale, offset_h, offset_w):
 
         # gt_boxes_input
         bboxes = tf.stack(
             [
-                bboxes[:, 0] * image_shape[1] * scale + tf.cast(offset_w, dtype=tf.float32),
-                bboxes[:, 1] * image_shape[0] * scale + tf.cast(offset_h, dtype=tf.float32),
-                bboxes[:, 2] * image_shape[1] * scale + tf.cast(offset_w, dtype=tf.float32),
-                bboxes[:, 3] * image_shape[0] * scale + tf.cast(offset_h, dtype=tf.float32),
+                bboxes[:, 0] * scale + tf.cast(offset_w, dtype=tf.float32),
+                bboxes[:, 1] * scale + tf.cast(offset_h, dtype=tf.float32),
+                bboxes[:, 2] * scale + tf.cast(offset_w, dtype=tf.float32),
+                bboxes[:, 3] * scale + tf.cast(offset_h, dtype=tf.float32),
                 classes
             ],
             axis=-1,
@@ -251,25 +182,38 @@ def preprocess_data(phi=0, mode: str = "ResNetV1", fmap_shapes=None, max_bboxes:
 
     def _preprocess_data(sample):
         #
-        image = tf.cast(sample["image"], dtype=tf.float32)
-        image_shape = tf.cast(tf.shape(image)[:2], dtype=tf.float32)
-        bboxes = tf.cast(sample["objects"]["bbox"], dtype=tf.float32)
-        classes = tf.cast(sample["objects"]["label"], dtype=tf.float32)
+        image, image_shape, bboxes, classes = compute_inputs(sample)
 
         # TODO: data augmentation
-        image, bboxes = random_flip_horizontal(image, bboxes)
-
-        # image, bboxes = tf_rotate(image, bboxes, image_shape, prob=0.5)
-        # image, bboxes = crop(image, bboxes)
+        image, bboxes = tf_rotate(image, image_shape, bboxes, prob=0.5)
+        image, bboxes = random_flip_horizontal(image, image_shape, bboxes, prob=0.5)
 
         #
         image, scale, offset_h, offset_w = _resize_image(image=image, target_size=_image_size[phi])
         image = _normalization_image(image, mode)
-        bboxes, bboxes_count = _padding_bboxes(image_shape, bboxes, classes, scale, offset_h, offset_w)
+        bboxes, bboxes_count = _padding_bboxes(bboxes, classes, scale, offset_h, offset_w)
         fmaps_shape = tf.constant(fmap_shapes, dtype=tf.int32)
         return image, bboxes, bboxes_count[None], fmaps_shape
 
     return _preprocess_data
+
+
+def compute_inputs(sample):
+    image = tf.cast(sample["image"], dtype=tf.float32)
+    image_shape = tf.cast(tf.shape(image)[:2], dtype=tf.float32)
+    bboxes = tf.cast(sample["objects"]["bbox"], dtype=tf.float32)
+    classes = tf.cast(sample["objects"]["label"], dtype=tf.float32)
+
+    bboxes = tf.stack(
+        [
+            bboxes[:, 0] * image_shape[1],
+            bboxes[:, 1] * image_shape[0],
+            bboxes[:, 2] * image_shape[1],
+            bboxes[:, 3] * image_shape[0],
+        ],
+        axis=-1
+    )
+    return image, image_shape, bboxes, classes
 
 
 def inputs_targets(image, bboxes, bboxes_count, fmaps_shape):
@@ -320,7 +264,7 @@ def create_pipeline_test(phi=0, mode="ResNetV1", db="DPCB", batch_size=1):
     autotune = tf.data.AUTOTUNE
 
     if db == "DPCB":
-        (train, test) = tfds.load(name="dpcb_db", split=["train", "test"], data_dir="D:/datasets/")
+        (train, test) = tfds.load(name="dpcb_db", split=["train", "test"], data_dir="D:/huai/datasets/")
     else:
         train = None
         test = None

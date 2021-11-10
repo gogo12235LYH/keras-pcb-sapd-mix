2957221a7c920ec3025427bef023a181f06969b3
gogo12235LYH <dogfish87@gmail.com>
2021-11-10 13:56:01 +0800

    move backbone to models.backbone

--- backbone/__init__.py
+++
@@ -1,9 +0,0 @@
-from .resnet_V2 import (
-    ResNet2D50,
-    ResNet2D101
-)
-
-
-# 2020-10-22, ResNet with Group Normalization
-ResNet50 = ResNet2D50
-ResNet101 = ResNet2D101
---
+++ models/backbone/__init__.py
@@ -0,0 +1,4 @@
+from .resnet import ResNet2D50, ResNet2D101
+
+ResNet50 = ResNet2D50
+ResNet101 = ResNet2D101
